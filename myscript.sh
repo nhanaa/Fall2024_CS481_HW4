6a39aea114d7cc40d4629cdabd3a027025f02cc4
Class account - UAL <ualclsd0185@asaxlogin3.asc.edu>
2024-11-14 14:07:52 -0600

    Add test correctness job files

--- myscript.sh
+++ myscript.sh
@@ -4,20 +4,14 @@ module load openmpi/4.1.4-gcc11
 
 mpic++ -g -Wall -O -o hw4 hw4.cpp
 
-mpirun -n 8 hw4 5000 5000 8 /scratch/$USER
-mpirun -n 8 hw4 5000 5000 8 /scratch/$USER
-mpirun -n 8 hw4 5000 5000 8 /scratch/$USER
+mpirun -n 2 hw4 100 100 2 /scratch/$USER
+mpirun -n 4 hw4 100 100 8 /scratch/$USER
+mpirun -n 8 hw4 100 100 8 /scratch/$USER
+mpirun -n 16 hw4 100 100 16 /scratch/$USER
 
-mpirun -n 10 hw4 5000 5000 10 /scratch/$USER
-mpirun -n 10 hw4 5000 5000 10 /scratch/$USER
-mpirun -n 10 hw4 5000 5000 10 /scratch/$USER
-
-mpirun -n 16 hw4 5000 5000 16 /scratch/$USER
-mpirun -n 16 hw4 5000 5000 16 /scratch/$USER
-mpirun -n 16 hw4 5000 5000 16 /scratch/$USER
-
-mpirun -n 20 hw4 5000 5000 20 /scratch/$USER
-mpirun -n 20 hw4 5000 5000 20 /scratch/$USER
-mpirun -n 20 hw4 5000 5000 20 /scratch/$USER
+diff /scratch/$USER/output100.100.2.0 /scratch/$USER/output100.100.4.0
+diff /scratch/$USER/output100.100.2.0 /scratch/$USER/output100.100.8.0
+diff /scratch/$USER/output100.100.4.0 /scratch/$USER/output100.100.8.0
+diff /scratch/$USER/output100.100.8.0 /scratch/$USER/output100.100.16.0
 
 module purge
